f4d999864e90dd2bb7dc96a44ffb1f278e245f6c
Ray.Lin <Ray.Lin@chinasoftinc.com>
2018-05-29 16:49:38 +0800

    删除

--- src/services/system/users.js
+++
@@ -1,5 +0,0 @@
-import request from '../../utils/request';
-
-export function fetch({ pageSize,pageNum,keyWord }) {
-  return request(`/api/user/getByPage?pageNum=${pageNum}&pageSize=${pageSize}&keyWord=${keyWord}`);
-}
